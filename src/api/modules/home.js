fdab442941782e802b93aebe5f9742949708d71d
gongpengji <gongpengji@hzyushi.cn>
2019-11-01 10:41:46 +0800

    a

--- src/api/modules/home.js
+++
@@ -1,7 +0,0 @@
-/**
- * home模块所有的接口
- */
-const HOME = {
-
-}
-export default HOME
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import Offline from './Offline'
-[Offline].forEach(item => {
-    Vue.component(item.name, item)
-})
